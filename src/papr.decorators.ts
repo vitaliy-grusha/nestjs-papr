23b6f94fb9bf1bac2faac455e4cbb35e5e0f7af7
Vitaliy Grusha <vit.grusha@gmail.com>
2023-05-22 18:34:13 +0300

    Fix Inject

--- src/papr.decorators.ts
+++ src/papr.decorators.ts
@@ -3,13 +3,17 @@ import { Inject } from '@nestjs/common';
 import { getConnectionToken, getPaprToken, getModelToken } from './papr.utils';
 import { ModelDef } from './papr.types';
 
+interface InjectResult {
+  (target: object, key: string | symbol | undefined, index?: number): void;
+}
+
 export const InjectModel = (
   { collection }: ModelDef<any, any, any>,
   connectionName?: string,
-) => Inject(getModelToken(collection, connectionName));
+): InjectResult => Inject(getModelToken(collection, connectionName));
 
-export const InjectPapr = (connectionName?: string) =>
+export const InjectPapr = (connectionName?: string): InjectResult =>
   Inject(getPaprToken(connectionName));
 
-export const InjectConnection = (name?: string) =>
+export const InjectConnection = (name?: string): InjectResult =>
   Inject(getConnectionToken(name));
